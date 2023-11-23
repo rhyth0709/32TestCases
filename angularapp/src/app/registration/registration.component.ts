510a39ac6f58a008fbaf0a08ab34da918bbd8e35
Iamneo <techadmin@iamneo.ai>
2023-11-23 17:30:21 +0000

    Backup Commit

--- angularapp/src/app/registration/registration.component.ts
+++ angularapp/src/app/registration/registration.component.ts
@@ -7,40 +7,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-  // registrationForm: FormGroup;
 
-  // constructor(private formBuilder: FormBuilder) {
-  //   this.registrationForm = this.formBuilder.group({
-  //     username: ['', Validators.required],
-  //     password: ['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]],
-  //     confirmPassword: ['', Validators.required]
-  //   });
-  // }
   ngOnInit(): void {
   }
 
-  // register() {
-  //   if (this.registrationForm.valid) {
-  //     // Perform registration logic here
-  //     const username = this.registrationForm.get('username')?.value;
-  //     const password = this.registrationForm.get('password')?.value;
-  //     const confirmPassword = this.registrationForm.get('confirmPassword')?.value;
-
-  //     // Additional logic (e.g., calling a registration service) goes here
-  //     console.log('Registration successful!', username, password, confirmPassword);
-  //   } else {
-  //     // Mark form controls as touched to trigger validation messages
-  //     this.markFormGroupTouched(this.registrationForm);
-  //   }
-  }
-
-  // private markFormGroupTouched(formGroup: FormGroup) {
-  //   (Object as any).values(formGroup.controls).forEach(control => {
-  //     control.markAsTouched();
-
-  //     if (control instanceof FormGroup) {
-  //       this.markFormGroupTouched(control);
-  //     }
-  //   });
-  // }
-//}
+}
